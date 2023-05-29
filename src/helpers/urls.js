59c1f78e507e26f598b9edcde3064b46792017ec
Prashant Kumar <prashant.aman9@gmail.com>
2023-05-29 17:49:37 +0530

    Remove .env imports

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://api.themoviedb.org/3'
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+// const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+const API_KEY = '44093fe7919d31a674b7b8d0d4b8ba25'
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/original'
 
 export const APIurls = {
