ec7d8db7be6e91d9f9e71459a454fa6f6047e0a8
Prashant Kumar <prashant.aman9@gmail.com>
2023-06-01 17:12:33 +0530

    Without font-family

--- src/global-styles.js
+++ src/global-styles.js
@@ -8,7 +8,7 @@ const GlobalStyle = createGlobalStyle`
     html,body{
         margin: 0;
         padding: 0;
-        font-family: 'Netflix sans','Helvetica Neue',Helvetica,Arial, sans-serif;
+        font-family: sans-serif;
         -webkit-font-smoothing:antialiased;
         -moz-osx-font-smoothing:grayscale;
         background-color: black;
